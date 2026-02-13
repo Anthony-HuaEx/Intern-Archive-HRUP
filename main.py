556ae8907b5263b70b223a4ff6875ea840d098e8
SleepyPandas <tommyrobotics1@gmail.com>
2026-02-13 15:11:58 -0500

    Script to convert to PDF

---
+++ main.py
@@ -0,0 +1,9 @@
+from seamless_pdf import convert
+
+
+
+convert(r"newsletters\January Intern NL.pdf", r"newsletters\January Intern NL.html")
+
+convert(r"newsletters\Feb Intern NL.pdf", r"newsletters\Feb Intern NL.html")
+
+convert(r"newsletters\NewGrad Feb.pdf", r"newsletters\NewGrad Feb.html")
